d61b60c9004df3cc59fe50160a9e841ee145e7cd
mo <pitipon@gmail.com>
2025-08-07 18:10:06 +0800

    config loading

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pitipon/shop-microservices
 
 go 1.24.0
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/pitipon/shop-microservices/config"
+)
+
+func main() {
+	ctx := context.Background()
+	_ = ctx
+
+	// Initialize config
+	cfg := config.LoadConfig(func() string {
+		if len(os.Args) < 2 {
+			log.Fatal("Error: .env path is required")
+		}
+		fmt.Println("Using config file:", os.Args[1])
+		return os.Args[1]
+	}())
+
+	log.Println(cfg)
 }
