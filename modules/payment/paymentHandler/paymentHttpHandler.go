779b5af9a5ce9885d77167bbae8ad3a545d25be7
mo <pitipon@gmail.com>
2025-08-23 14:24:14 +0800

    payment handler init

--- modules/payment/paymentHandler/paymentHttpHandler.go
+++ modules/payment/paymentHandler/paymentHttpHandler.go
@@ -1,1 +1,22 @@
 package paymentHandler
+
+import (
+	"github.com/pitipon/shop-microservices/config"
+	"github.com/pitipon/shop-microservices/modules/payment/paymentUsecase"
+)
+
+type (
+	PaymentHttpHandlerService interface{}
+
+	paymentHttpHandler struct {
+		cfg            *config.Config
+		paymentUsecase paymentUsecase.PaymentUsecaseService
+	}
+)
+
+func NewPaymentHttpHandler(cfg *config.Config, paymentUsecase paymentUsecase.PaymentUsecaseService) PaymentHttpHandlerService {
+	return &paymentHttpHandler{
+		cfg:            cfg,
+		paymentUsecase: paymentUsecase,
+	}
+}
--- modules/payment/paymentUsecase/paymentUsecase.go
+++ modules/payment/paymentUsecase/paymentUsecase.go
@@ -1,1 +1,17 @@
 package paymentUsecase
+
+import "github.com/pitipon/shop-microservices/modules/payment/paymentRepository"
+
+type (
+	PaymentUsecaseService interface{}
+
+	paymentUsecase struct {
+		paymentRepository paymentRepository.PaymentRepositoryService
+	}
+)
+
+func NewPaymentUsecase(paymentRepository paymentRepository.PaymentRepositoryService) PaymentUsecaseService {
+	return &paymentUsecase{
+		paymentRepository: paymentRepository,
+	}
+}
