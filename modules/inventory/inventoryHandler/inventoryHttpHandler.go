58966fe4aa3ad76ad1679e0d5d01afba3add4a56
mo <pitipon@gmail.com>
2025-08-23 13:58:29 +0800

    inventory handler init

--- modules/inventory/inventoryHandler/inventoryHttpHandler.go
+++ modules/inventory/inventoryHandler/inventoryHttpHandler.go
@@ -1,1 +1,22 @@
 package inventoryHandler
+
+import (
+	"github.com/pitipon/shop-microservices/config"
+	"github.com/pitipon/shop-microservices/modules/inventory/inventoryUsecase"
+)
+
+type (
+	InventoryHttpHandlerService interface{}
+
+	inventoryHttpHandler struct {
+		cfg              *config.Config
+		inventoryUsecase inventoryUsecase.InventoryUsecaseService
+	}
+)
+
+func NewInventoryHttpHandler(cfg *config.Config, inventoryUsecase inventoryUsecase.InventoryUsecaseService) InventoryHttpHandlerService {
+	return &inventoryHttpHandler{
+		cfg:              cfg,
+		inventoryUsecase: inventoryUsecase,
+	}
+}
--- modules/inventory/inventoryUsecase/inventoryUsecase.go
+++ modules/inventory/inventoryUsecase/inventoryUsecase.go
@@ -1,1 +1,17 @@
 package inventoryUsecase
+
+import "github.com/pitipon/shop-microservices/modules/inventory/inventoryRepository"
+
+type (
+	InventoryUsecaseService interface{}
+
+	inventoryUsecase struct {
+		inventoryRepository inventoryRepository.InventoryRepositoryService
+	}
+)
+
+func NewInventoryUsecase(inventoryRepository inventoryRepository.InventoryRepositoryService) InventoryUsecaseService {
+	return &inventoryUsecase{
+		inventoryRepository: inventoryRepository,
+	}
+}
