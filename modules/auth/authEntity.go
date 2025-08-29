b500756341e970274418739322b98990939125f7
mo <pitipon@gmail.com>
2025-08-29 17:26:28 +0800

    auth and player entity and model

--- modules/auth/authEntity.go
+++ modules/auth/authEntity.go
@@ -1,1 +1,24 @@
 package auth
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type (
+	Credential struct {
+		Id          bson.ObjectID `json:"_id" bson:"_id,omitempty"`
+		PlayerId    string        `json:"player_id" bson:"player_id"`
+		RoleCode    int           `json:"role_code" bson:"role_code"`
+		AccessToken string        `json:"access_token" bson:"access_token"`
+		CreatedAt   time.Time     `json:"created_at"`
+		UpdatedAt   time.Time     `json:"updated_at"`
+	}
+
+	Role struct {
+		Id    bson.ObjectID `json:"_id" bson:"_id,omitempty"`
+		Title string        `json:"title" bson:"title"`
+		Code  int           `json:"code" bson:"code"`
+	}
+)
