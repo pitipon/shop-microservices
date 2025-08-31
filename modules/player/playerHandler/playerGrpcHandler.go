a9a32fc29835a128f241b95d86f3ceffe15aab39
mo <pitipon@gmail.com>
2025-08-31 13:07:05 +0800

    proto player

--- modules/player/playerHandler/playerGrpcHandler.go
+++ modules/player/playerHandler/playerGrpcHandler.go
@@ -1,9 +1,15 @@
 package playerHandler
 
-import "github.com/pitipon/shop-microservices/modules/player/playerUsecase"
+import (
+	"context"
+
+	playerPb "github.com/pitipon/shop-microservices/modules/player/playerPb"
+	"github.com/pitipon/shop-microservices/modules/player/playerUsecase"
+)
 
 type (
 	playerGrpcHandler struct {
+		playerPb.UnimplementedPlayerGrpcServiceServer
 		playerUsecase playerUsecase.PlayerUsecaseService
 	}
 )
@@ -12,4 +18,16 @@ func NewPlayerGrpcHandler(playerUsecase playerUsecase.PlayerUsecaseService) *pla
 	return &playerGrpcHandler{
 		playerUsecase: playerUsecase,
 	}
+}
+
+func CredentialSearch(ctx context.Context, req *playerPb.CredentialSearchReq) (*playerPb.PlayerProfile, error) {
+	return nil, nil
+}
+
+func FindOnePlayerProfileToRefresh(ctx context.Context, req *playerPb.FindOnePlayerProfileToRefreshReq) (*playerPb.PlayerProfile, error) {
+	return nil, nil
+}
+
+func GetPlayerSavingAccount(ctx context.Context, req *playerPb.GetPlayerSavingAccountReq) (*playerPb.GetPlayerSavingAccountRes, error) {
+	return nil, nil
 }
