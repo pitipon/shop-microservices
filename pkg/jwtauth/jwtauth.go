c66bc61f83ae0318e6d05031dfc1605eb71153c7
mo <pitipon@gmail.com>
2025-09-26 23:26:11 +0700

    save

---
+++ pkg/jwtauth/jwtauth.go
@@ -0,0 +1,142 @@
+package jwtauth
+
+import (
+	"math"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type (
+	AuthFactory interface {
+		SignToken() string
+	}
+
+	Claims struct {
+		Id       string `json:"id"`
+		RoleCode int    `json:"role_code"`
+	}
+
+	AuthMapClaims struct {
+		*Claims
+		jwt.RegisteredClaims
+	}
+
+	authConcrete struct {
+		Secret []byte
+		Claims *AuthMapClaims `json:"claims"`
+	}
+
+	accessToken struct {
+		*authConcrete
+	}
+
+	refreshToken struct {
+		*authConcrete
+	}
+
+	apiKey struct {
+		*authConcrete
+	}
+)
+
+func (a *authConcrete) SignToken() string {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, a.Claims)
+
+	ss, _ := token.SignedString(a.Secret)
+
+	return ss
+}
+
+func now() time.Time {
+	loc, _ := time.LoadLocation("Asia/Bangkok")
+	return time.Now().In(loc)
+}
+
+// t is a second
+func jwtTimeDurationCal(t int64) *jwt.NumericDate {
+	loc, _ := time.LoadLocation("Asia/Bangkok")
+	return jwt.NewNumericDate(time.Now().In(loc).Add(time.Duration(t * int64(math.Pow10(9)))))
+}
+
+func jwtTimeRepeatAdapter(t int64) *jwt.NumericDate {
+	return jwt.NewNumericDate(time.Unix(t, 0))
+}
+
+func NewAccessToken(secret string, expiredAt int64, claims *Claims) AuthFactory {
+	return &accessToken{
+		authConcrete: &authConcrete{
+			Secret: []byte(secret),
+			Claims: &AuthMapClaims{
+				Claims: claims,
+				RegisteredClaims: jwt.RegisteredClaims{
+					Issuer:    "shop-miceoservices",
+					Subject:   "access-token",
+					Audience:  []string{"shop-microservices"},
+					ExpiresAt: jwtTimeDurationCal(expiredAt),
+					NotBefore: jwt.NewNumericDate(now()),
+					IssuedAt:  jwt.NewNumericDate(now()),
+				},
+			},
+		},
+	}
+}
+
+func NewRefreshToken(secret string, expiredAt int64, claims *Claims) AuthFactory {
+	return &refreshToken{
+		authConcrete: &authConcrete{
+			Secret: []byte(secret),
+			Claims: &AuthMapClaims{
+				Claims: claims,
+				RegisteredClaims: jwt.RegisteredClaims{
+					Issuer:    "shop-miceoservices",
+					Subject:   "refresh-token",
+					Audience:  []string{"shop-microservices"},
+					ExpiresAt: jwtTimeDurationCal(expiredAt),
+					NotBefore: jwt.NewNumericDate(now()),
+					IssuedAt:  jwt.NewNumericDate(now()),
+				},
+			},
+		},
+	}
+}
+
+func ReloadToken(secret string, expiredAt int64, claims *Claims) string {
+	obj := &refreshToken{
+		authConcrete: &authConcrete{
+			Secret: []byte(secret),
+			Claims: &AuthMapClaims{
+				Claims: claims,
+				RegisteredClaims: jwt.RegisteredClaims{
+					Issuer:    "shop-miceoservices",
+					Subject:   "refresh-token",
+					Audience:  []string{"shop-microservices"},
+					ExpiresAt: jwtTimeRepeatAdapter(expiredAt),
+					NotBefore: jwt.NewNumericDate(now()),
+					IssuedAt:  jwt.NewNumericDate(now()),
+				},
+			},
+		},
+	}
+
+	return obj.SignToken()
+}
+
+func NewApiKey(secret string, expiredAt int64, claims *Claims) AuthFactory {
+	return &apiKey{
+		authConcrete: &authConcrete{
+			Secret: []byte(secret),
+			Claims: &AuthMapClaims{
+				Claims: claims,
+				RegisteredClaims: jwt.RegisteredClaims{
+					Issuer:    "shop-miceoservices",
+					Subject:   "api-key",
+					Audience:  []string{"shop-microservices"},
+					ExpiresAt: jwtTimeDurationCal(31560000),
+					NotBefore: jwt.NewNumericDate(now()),
+					IssuedAt:  jwt.NewNumericDate(now()),
+				},
+			},
+		},
+	}
+}
